2d6288d916a4600c7a2e0770ad33cef20e5b3701
firman196 <firmanagussaputro970@gmail.com>
2023-06-16 18:26:16 +0700

    add base layout themplate

---
+++ resources/views/pelanggan.blade.php
@@ -0,0 +1,18 @@
+@extends('layouts.app')
+
+@section('content')
+<body class="antialiased">
+    <div class="mt-16 px-5">
+        <div class="bg-white shadow-sm p-4 ">
+            <div class="d-flex justify-content-between">
+                <div>Daftar Pelanggan</div>
+                <button class="btn btn-dark rounded-pill px-4 py-2">
+                    <img src="/image/icon/create.svg" alt="create" srcset="" class="me-2">
+                    Create Pelanggan
+                </button>
+            </div>
+            tes
+        </div>
+    </div>
+</body>
+@endsection
